2faabe915cd8801c4a3392a538ae2b164831351d
Luke Evers <me@lukevers.com>
2016-04-06 08:25:54 -0400

    Add function comments to *Text

--- src/arp147/ui/text/text.go
+++ src/arp147/ui/text/text.go
@@ -19,11 +19,13 @@ type Text struct {
 	control *TextControlSystem
 }
 
+// Create a new *Text
 func New(t Text) *Text {
 	t.control = &TextControlSystem{}
 	return &t
 }
 
+// Convert *Text to a usable *ecs.Entity
 func (t *Text) Entity(w *ecs.World) *ecs.Entity {
 	font := &engo.Font{
 		Size: t.Size,
@@ -55,34 +57,42 @@ func (t *Text) Entity(w *ecs.World) *ecs.Entity {
 	return entity
 }
 
+// Callback function for when the mouse clicked text
 func (t *Text) OnClicked(fn func(entity *ecs.Entity, dt float32)) {
 	t.control.Clicked = fn
 }
 
+// Callback function for when the mouse released text
 func (t *Text) OnReleased(fn func(entity *ecs.Entity, dt float32)) {
 	t.control.Released = fn
 }
 
+// Callback function for when the mouse hovered text
 func (t *Text) OnHovered(fn func(entity *ecs.Entity, dt float32)) {
 	t.control.Hovered = fn
 }
 
+// Callback function for when the mouse dragged text
 func (t *Text) OnDragged(fn func(entity *ecs.Entity, dt float32)) {
 	t.control.Dragged = fn
 }
 
+// Callback function for when the mouse right clicked text
 func (t *Text) OnRightClicked(fn func(entity *ecs.Entity, dt float32)) {
 	t.control.RightClicked = fn
 }
 
+// Callback function for when the mouse right released text
 func (t *Text) OnRightReleased(fn func(entity *ecs.Entity, dt float32)) {
 	t.control.RightReleased = fn
 }
 
+// Callback function for when the mouse enters text
 func (t *Text) OnEnter(fn func(entity *ecs.Entity, dt float32)) {
 	t.control.Enter = fn
 }
 
+// Callback function for when the mouse leaves text
 func (t *Text) OnLeave(fn func(entity *ecs.Entity, dt float32)) {
 	t.control.Leave = fn
 }
