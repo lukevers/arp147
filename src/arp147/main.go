c70005905a838b0399c105c477a578a461ca3db9
Luke Evers <luke@lukevers.com>
2017-05-09 08:26:18 -0400

    Catch all panics and log them

--- src/arp147/main.go
+++ src/arp147/main.go
@@ -2,17 +2,26 @@ package main
 
 import (
 	_ "arp147/flags"
-	_ "arp147/logging"
+	"arp147/logging"
 	"arp147/scenes"
 	"engo.io/engo"
 )
 
 func main() {
+	// Catch all panics and log them.
+	defer func() {
+		if r := recover(); r != nil {
+			logging.Stderr.Fatal(r)
+		}
+	}()
+
+	// Setup our run options
 	options := engo.RunOptions{
 		Title:  "Arp 147",
 		Width:  1200,
 		Height: 800,
 	}
 
+	// Run the game
 	engo.Run(options, &scenes.DefaultScene{})
 }
