ee717d28bec2f4423fff2dbb1fb40d8bf857d6f4
Luke Evers <luke@lukevers.com>
2019-03-09 11:32:40 -0500

    Remove old interface, and rename file

--- viewers/view.go
+++ viewers/view.go
@@ -7,16 +7,6 @@ import (
 	"github.com/lukevers/arp147/ui"
 )
 
-// type Viewer interface {
-// 	SetActiveTab(*ui.Text)
-// 	GetActiveTab() *ui.Text
-// 	GetTabs() []*ui.Text
-
-// 	GetActivePane() *Pane
-// 	SetActivePane(*Pane)
-// 	GetPanes() map[string]*Pane
-// }
-
 type View struct {
 	tab  *ui.Text
 	tabs []*ui.Text
